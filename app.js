9cdfb237dc1a0d1c30ff531a3d5e3000ad1c2d6a
xzj66778899 <xzj66778899@gmail.com>
2023-08-23 07:16:12 +1000

    edit .env.sample

--- app.js
+++ app.js
@@ -10,7 +10,10 @@ const app = express()
 
 app.use(cookieParser())
 
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true
+  }))
 
 app.use(express.json())
 
