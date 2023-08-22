97452574dadfd62b1714d345507b2ba13ee57ec4
xzj66778899 <xzj66778899@gmail.com>
2023-08-22 11:11:11 +1000

    added JWT Token when login, added cookies with Token response to frontend, add user/checkauthentication route

--- app.js
+++ app.js
@@ -1,8 +1,14 @@
 import express from 'express'
 import cors from 'cors'
+import cookieParser from 'cookie-parser'
 import authRoutes from './routes/authRoute.js'
+import usersRoutes from './routes/usersRoute.js'
+
+
 
 const app = express()
+
+app.use(cookieParser())
 
 app.use(cors())
 
@@ -11,6 +17,7 @@ app.use(express.json())
 app.get('/', app.get('/', (request, response) => response.send({ info: 'API!' })))
 
 app.use('/auth', authRoutes)
+app.use('/user', usersRoutes)
 
 
 
