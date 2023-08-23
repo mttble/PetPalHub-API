bb5ae605d626de9116ddc569bab6d7897aab50e2
xzj66778899 <xzj66778899@gmail.com>
2023-08-23 10:29:06 +1000

    drier the register funciton

--- controllers/auth.js
+++ controllers/auth.js
@@ -10,113 +10,58 @@ export const test = async (req, res, next) => {
 }
 
 export const register = async (req, res, next) => {
-   // when user register as User
-    if (req.body.isCarer === false){ 
-        try {
-            const emailExists = await checkExistingEmail(req.body.email, UserModel);
-            if (emailExists) {
-                return res.json({error: "Email already exists as User"});
-            }
-
-            const hashedPassword = await validateAndHashPassword(req.body.password);
-            validateDateOfBirth(req.body.dateOfBirth)
-            if (hashedPassword === false) {
-                return res.json({error: "Password must contain: at least one lowercase letter, one uppercase letter, one number, one special character and be at least 8 characters in length"})
-            }
-
-            let parsedDate
-            if (req.body.dateOfBirth) {
-                parsedDate = moment.utc(req.body.dateOfBirth, "DD/MM/YYYY").toDate()}
-            else {parsedDate = undefined}
-            
-
-            const newUser = new UserModel({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hashedPassword,
-                dateOfBirth: parsedDate,
-                profileImage: req.body.profileImage,
-                address: {
-                    street: req.body.address?.street,
-                    city: req.body.address?.city,
-                    state: req.body.address?.state,
-                    postalCode: req.body.address?.postalCode,
-                    country: req.body.address?.country
-                },
-                phoneNumber: req.body.phoneNumber,
-                bio: req.body.bio,
-                isCarer: req.body.isCarer
-            });
-
-            // Save the new user to the database
-            const savedUser = await newUser.save();
-            const userObject = savedUser.toObject(); // Convert the Mongoose document to a plain JavaScript object
-            if (userObject.dateOfBirth) {
-                userObject.dateOfBirth = moment.utc(userObject.dateOfBirth).format('DD/MM/YYYY');
-            }
-            delete userObject.password; // Remove the password
-            res.status(201).json({ message: "User registered successfully!", user: userObject });
-
-        } catch (error) {
-            console.error("Error:", error);
-            // Catch any errors and send an error response
-            res.status(500).json({ message: "Error registering the user", error: error.message });
-        }}
-    // when user register as Carer
-    if (req.body.isCarer === true){ 
-        try {
-            const emailExists = await checkExistingEmail(req.body.email, CarerModel);
-            if (emailExists) {
-                return res.json({error: "Email already exists as Carer"});
-            }
-
-            const hashedPassword = await validateAndHashPassword(req.body.password);
-            validateDateOfBirth(req.body.dateOfBirth)
-            if (hashedPassword === false) {
-                return res.json({error: "Password must contain: at least one lowercase letter, one uppercase letter, one number, one special character and be at least 8 characters in length"})
-            }
-
-            let parsedDate
-            if (req.body.dateOfBirth) {
-                parsedDate = moment.utc(req.body.dateOfBirth, "DD/MM/YYYY").toDate()}
-            else {parsedDate = undefined}
-
-            const newCarer = new CarerModel({
-                firstName: req.body.firstName,
-                lastName: req.body.lastName,
-                email: req.body.email,
-                password: hashedPassword,
-                dateOfBirth: parsedDate,
-                profilepImage: req.body.profileImage,
-                address: {
-                    street: req.body.address?.street,
-                    city: req.body.address?.city,
-                    state: req.body.address?.state,
-                    postalCode: req.body.address?.postalCode,
-                    country: req.body.address?.country
-                },
-                phoneNumber: req.body.phoneNumber,
-                bio: req.body.bio,
-                isCarer: req.body.isCarer
-            });
+    const Model = req.body.role === 'user' ? UserModel : CarerModel;
+    const roleMessage = req.body.role === 'user' ? 'User' : 'Carer';
     
-                // Save the new carer to the database
-                const savedCarer = await newCarer.save();
-                const carerObject = savedCarer.toObject(); // Convert the Mongoose document to a plain JavaScript object
-                if (carerObject.dateOfBirth) {
-                    carerObject.dateOfBirth = moment.utc(carerObject.dateOfBirth).format('DD/MM/YYYY');
-                }
-                delete carerObject.password; // Remove the password
-                res.status(201).json({ message: "Carer registered successfully!", carer: carerObject });
-    
-            } catch (error) {
-                console.error("Error:", error);
-                // Catch any errors and send an error response
-                res.status(500).json({ message: "Error registering the user", error: error.message });
-            }}
+    try {
+        const emailExists = await checkExistingEmail(req.body.email, Model);
+        if (emailExists) {
+            return res.json({error: `Email already exists as ${roleMessage}`});
+        }
 
+        const hashedPassword = await validateAndHashPassword(req.body.password);
+        validateDateOfBirth(req.body.dateOfBirth);
+        
+        if (hashedPassword === false) {
+            return res.json({error: "Password must contain: at least one lowercase letter, one uppercase letter, one number, one special character and be at least 8 characters in length"});
+        }
 
+        let parsedDate = req.body.dateOfBirth ? moment.utc(req.body.dateOfBirth, "DD/MM/YYYY").toDate() : undefined;
+
+        const details = {
+            firstName: req.body.firstName,
+            lastName: req.body.lastName,
+            email: req.body.email,
+            password: hashedPassword,
+            dateOfBirth: parsedDate,
+            profileImage: req.body.profileImage,
+            address: {
+                street: req.body.address?.street,
+                city: req.body.address?.city,
+                state: req.body.address?.state,
+                postalCode: req.body.address?.postalCode,
+                country: req.body.address?.country
+            },
+            phoneNumber: req.body.phoneNumber,
+            bio: req.body.bio,
+            role: req.body.role
+        };
+
+        const newRegister = new Model(details);
+        const savedRegister = await newRegister.save();
+
+        const registerObject = savedRegister.toObject();
+        if (registerObject.dateOfBirth) {
+            registerObject.dateOfBirth = moment.utc(registerObject.dateOfBirth).format('DD/MM/YYYY');
+        }
+        delete registerObject.password;
+
+        res.status(201).json({ message: `${roleMessage} registered successfully!`, register: registerObject });
+
+    } catch (error) {
+        console.error("Error:", error);
+        res.status(500).json({ message: `Error registering the ${roleMessage}`, error: error.message });
+    }
 }
 
 
