4b0fc8a0d8de0d5b1ef4ba635a03179b18126075
mttble <matthew.beale1@hotmail.com>
2023-08-27 13:31:12 +0930

    added get route for pet profiles so view pets can display users pets and also adjusted pet model to include petType and users id

--- routes/petsRoute.js
+++ routes/petsRoute.js
@@ -8,20 +8,32 @@ import { PetModel } from '../models/Pet.js';
 const router = express.Router();
 
 
-router.post('/profile',verifyToken, petUpload.single('petImage'), async (req, res) => {
-    try {
+router.post('/profile', verifyToken, petUpload.single('petImage'), async (req, res) => {
+  try {
       const profileData = req.body;
       if (req.file) {
-        profileData.petImage = `/uploads/pet_image/${req.file.filename}`;
-        }
+          profileData.petImage = `/uploads/pet_image/${req.file.filename}`;
+      }
       const newPetProfile = new PetModel(profileData);
       await newPetProfile.save();
       res.status(201).send({ message: 'Profile created successfully!' });
-    } catch (error) {
-        console.error("Error while creating profile:", error);
-        res.status(500).send({ message: 'Failed to create profile.', error: error.message });
-      }
-  });
+  } catch (error) {
+      console.error("Error while creating profile:", error);
+      res.status(500).send({ message: 'Failed to create profile.', error: error.message });
+  }
+});
+
+// for users to view their pet profiles in account
+router.get('/pet-profiles', async (req, res) => {
+  try {
+    const userId = req.query.userId; // Get the userId from the query parameter
+    const petProfiles = await PetModel.find({ ownerId: userId }); // Fetch pet profiles owned by the specified user
+    res.json(petProfiles);
+  } catch (error) {
+    console.error('Error fetching pet profiles:', error);
+    res.status(500).send('Server Error');
+  }
+});
 
 
 export default router
